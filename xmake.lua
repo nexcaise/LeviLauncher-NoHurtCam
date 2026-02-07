f43e94163a504e315f7525f03b85376238454ec1
alvinqid <github@alvinq.my.id>
2026-02-07 21:49:40 +0700

    test

---
+++ xmake.lua
@@ -0,0 +1,52 @@
+set_project("NoHurtCam")
+set_version("1.0.0")
+
+set_languages("c99", "cxx20")
+
+add_rules("mode.release")
+
+add_cxflags(
+    "-O2",
+    "-fvisibility=hidden",
+    "-ffunction-sections",
+    "-fdata-sections",
+    "-w"
+)
+
+add_cflags(
+    "-O2",
+    "-fvisibility=hidden",
+    "-ffunction-sections",
+    "-fdata-sections",
+    "-w"
+)
+
+add_ldflags(
+    "-Wl,--gc-sections,--strip-all",
+    "-s",
+    {force = true}
+)
+
+add_requires(
+    "fmt 10.2.1",
+    "preloader-android git+https://github.com/nexcaise/preloader-android.git@main"
+)
+
+target("NoHurtCam")
+    set_kind("shared")
+    add_packages("fmt", "preloader-android")
+    add_files("src/main.cpp")
+    add_includedirs("src")
+    add_syslinks("log")
+    before_build(function (target)
+        local pkg = target:pkg("preloader-android")
+        if not pkg then return end
+        local logger = path.join(pkg:installdir(), "include/pl/Logger.h")
+        local data = io.readfile(logger)
+        if data then
+            data = data:gsub("#include <format>", "#include <fmt/format.h>")
+            data = data:gsub("std::vformat", "fmt::vformat")
+            data = data:gsub("std::make_format_args", "fmt::make_format_args")
+            io.writefile(logger, data)
+        end
+    end)
